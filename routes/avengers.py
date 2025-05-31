8c66fef99de5e6b98706f1926a8c2e8344837aae
Leanbrice <leagustin9@gmail.com>
2025-05-30 21:09:37 -0300

    subiendo arreglos

--- routes/avengers.py
+++ routes/avengers.py
@@ -4,7 +4,7 @@ avenger_bp = Blueprint("avenger_bp",__name__)
 from models.avenger import Avenger
 from flask import render_template,redirect,Blueprint,flash,request,url_for
 
-avenger_bp=Blueprint("avnger_bp",__name__)
+avenger_bp=Blueprint("avenger_bp",__name__)
 @avenger_bp.route("/", methods=["GET"])
 def get_avenger():
     avenger = Avenger.query.all()
@@ -26,12 +26,12 @@ def add_avenger():
         )
         db.session.add(nuevo_avenger)
         db.session.commit()
-        flash("Agente subido con exito","success")
+        flash("Avenger subido con exito","success")
         return redirect(url_for("avenger_bp.get_avenger"))
     except Exception as e:
         db.session.rollback()
         flash(f"error inesperado: {e}","danger")
-        return redirect(url_for("avenger_bp.get_avneger"))
+        return redirect(url_for("avenger_bp.get_avenger"))
 
 @avenger_bp.route("/avenger/edit/<int:id>",methods = ["GET","POST"])
 
@@ -43,6 +43,10 @@ def edit_avenger(id):
         alias= request.form.get("alias")
         habilidades = request.form.get("habilidades","")
         actor= request.form.get("actor")
+
+        if Avenger.query.filter_by(alias=alias).first():
+            flash("alias registrado","danger")
+            return redirect(request.referrer)
         try:
             avenger.nombre = nombre
             avenger.alias = alias
@@ -57,7 +61,7 @@ def edit_avenger(id):
             flash(f"error inesperado: {e}","danger")
             return redirect(url_for("avenger_bp.edit_avenger",id=id))
         
-    return render_template("agente/edit_avenger.html",avenger=avenger)
+    return render_template("avenger/edit_avenger.html",avenger=avenger)
 
 @avenger_bp.route("/avenger/delete/<int:id>" ,methods =["POST"])
 def delete_avenger(id):
@@ -65,7 +69,7 @@ def delete_avenger(id):
     try:
         db.session.delete(avenger)
         db.session.commit()
-        flash("agente eliminado","success")
+        flash("avnger eliminado","success")
         return redirect(url_for("avenger_bp.get_avenger"))
     except Exception as e:
         db.session.rollback()
